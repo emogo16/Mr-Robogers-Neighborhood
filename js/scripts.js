ecbd30d450bb409ee09ac718ef56d85634ebb2e7
Erika Mogollon <fany200025@hotmail.com>
2022-02-12 18:59:13 -0800

    it is a WIP

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//user interface logic
+$(document).ready(function() {
+  $("#form1").submit(function(event) {
+    event.preventDefault();
+    const nameinput = $(#name).;
+    const numinput = $(#number).;
+    
+  }
+}
